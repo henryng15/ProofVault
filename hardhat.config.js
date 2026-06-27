f1d25e7445a370c7ee0b9fd0175df5400ae79225
Vicientt <nguyen15@kenyon.edu>
2026-06-26 20:31:32 -0400

    feat: initialize Hardhat project with Solidity 0.8.28 compiler configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    amoy: {
+      url: `https://polygon-amoy.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80002,
+    },
+  },
 };
